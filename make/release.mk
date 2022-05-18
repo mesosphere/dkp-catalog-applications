133c02b4adbbdb0a17dedbe15782b73592044f9e
Grace Do <xgrace@gmail.com>
2022-05-18 11:01:44 -0700

    [release-2.2] build: cherry-pick bundle changes (#72)
    * build: Add chart bundle release targets (#63)
    
    * build: Add chart bundle release targets
    
    * ci: Add private go module config to Dockerfile
    
    * chore: logs release artifacts (#64)
    
    * feat: notify in slack about release (#66)
    
    * feat: notify in slack about release
    
    * chore: enable slack webhook env for ci run
    
    * chore: skip slack notification in case of dev release (#67)
    
    Co-authored-by: Casey Buto <cbuto@d2iq.com>
    Co-authored-by: Farhan <farhan5900@gmail.com>

--- make/release.mk
+++ make/release.mk
@@ -1,6 +1,7 @@
 BUILD_DIR := _build
 IMAGE_TAR_FILE := $(BUILD_DIR)/dkp-catalog-applications-image-bundle.tar.gz
 REPO_ARCHIVE_FILE := $(BUILD_DIR)/dkp-catalog-applications.tar.gz
+CHART_BUNDLE := $(BUILD_DIR)/dkp-catalog-applications-chart-bundle.tar.gz
 CATALOG_IMAGES_TXT := $(BUILD_DIR)/dkp_catalog_images.txt
 RELEASE_S3_BUCKET ?= downloads.mesosphere.io
 
@@ -27,12 +28,33 @@ else
 	                              helm-repositories services
 endif
 
+.PHONY: release.chart-bundle
+release.chart-bundle: kommander-cli
+	$(call print-target)
+	echo "Building charts bundle from dkp-catalog-applications repository: "
+	$(KOMMANDER_CLI_BIN) create chart-bundle \
+		--catalog-repository $(REPO_ROOT) \
+		--output $(CHART_BUNDLE)
+
 .PHONY: release.s3
+release.s3: CHART_BUNDLE_URL = https://downloads.d2iq.com/dkp/$(CATALOG_APPLICATIONS_VERSION)/dkp-catalog-applications-charts-bundle-$(CATALOG_APPLICATIONS_VERSION).tar.gz
+release.s3: REPO_ARCHIVE_URL = https://downloads.d2iq.com/dkp/$(CATALOG_APPLICATIONS_VERSION)/dkp-catalog-applications-$(CATALOG_APPLICATIONS_VERSION).tar.gz
+release.s3: IMAGE_BUNDLE_URL = https://downloads.d2iq.com/dkp/$(CATALOG_APPLICATIONS_VERSION)/dkp-catalog-applications-image-bundle-$(CATALOG_APPLICATIONS_VERSION).tar.gz
 release.s3:
 	$(call print-target)
 ifeq ($(CATALOG_APPLICATIONS_VERSION),"")
 	$(info CATALOG_APPLICATIONS_VERSION should be set to the version which is part of the s3 file path)
 else
+	aws s3 cp --no-progress --acl bucket-owner-full-control $(CHART_BUNDLE) s3://$(RELEASE_S3_BUCKET)/dkp/$(CATALOG_APPLICATIONS_VERSION)/dkp-catalog-applications-charts-bundle-$(CATALOG_APPLICATIONS_VERSION).tar.gz
+	echo "Published Chart Bundle to $(CHART_BUNDLE_URL)"
 	aws s3 cp --no-progress --acl bucket-owner-full-control $(REPO_ARCHIVE_FILE) s3://$(RELEASE_S3_BUCKET)/dkp/$(CATALOG_APPLICATIONS_VERSION)/dkp-catalog-applications-$(CATALOG_APPLICATIONS_VERSION).tar.gz
+	echo "Published Repo Archive File to $(REPO_ARCHIVE_URL)"
 	aws s3 cp --no-progress --acl bucket-owner-full-control $(IMAGE_TAR_FILE) s3://$(RELEASE_S3_BUCKET)/dkp/$(CATALOG_APPLICATIONS_VERSION)/dkp-catalog-applications-image-bundle-$(CATALOG_APPLICATIONS_VERSION).tar.gz
+	echo "Published Image Bundle to $(IMAGE_BUNDLE_URL)"
+ifeq (,$(findstring dev,$(CATALOG_APPLICATIONS_VERSION)))
+	# Make sure to set SLACK_WEBHOOK environment variable to webhook url for the below mentioned channel
+	curl -X POST -H 'Content-type: application/json' \
+	--data '{"channel":"#eng-shipit","blocks":[{"type":"header","text":{"type":"plain_text","text":":announce: DKP Catalog Applications $(CATALOG_APPLICATIONS_VERSION) is out!","emoji":true}},{"type":"section","text":{"type":"mrkdwn","text":"*Bundles:*\n:airgap: Airgapped Image Bundle: $(IMAGE_BUNDLE_URL)\n:package: Chart Bundle: $(CHART_BUNDLE_URL)\n:github: Git Repo Tarball: $(REPO_ARCHIVE_URL)"}}]}' \
+	$(SLACK_WEBHOOK)
+endif
 endif
