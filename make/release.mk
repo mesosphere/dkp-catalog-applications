81c3806e34ae417ecd52b4de3f1b573cde082ecc
Weiyanli Chen(York) <6115189+cwyl02@users.noreply.github.com>
2022-07-19 18:20:27 -0400

    build: add attribution file in release targets (#75)
    * build: add attribution file in release targets
    
    * build: add missing z flag to chart bundle cmd

--- make/release.mk
+++ make/release.mk
@@ -7,6 +7,8 @@ RELEASE_S3_BUCKET ?= downloads.mesosphere.io
 
 CATALOG_APPLICATIONS_VERSION ?= ""
 
+ATTRIBUTION_FILE_CONTENT = "For a full list of attributed 3rd party software, see https://d2iq.com/legal/3rd"
+
 $(BUILD_DIR):
 	@mkdir -p $(BUILD_DIR)
 
@@ -40,16 +42,21 @@ release.chart-bundle: kommander-cli
 release.s3: CHART_BUNDLE_URL = https://downloads.d2iq.com/dkp/$(CATALOG_APPLICATIONS_VERSION)/dkp-catalog-applications-charts-bundle-$(CATALOG_APPLICATIONS_VERSION).tar.gz
 release.s3: REPO_ARCHIVE_URL = https://downloads.d2iq.com/dkp/$(CATALOG_APPLICATIONS_VERSION)/dkp-catalog-applications-$(CATALOG_APPLICATIONS_VERSION).tar.gz
 release.s3: IMAGE_BUNDLE_URL = https://downloads.d2iq.com/dkp/$(CATALOG_APPLICATIONS_VERSION)/dkp-catalog-applications-image-bundle-$(CATALOG_APPLICATIONS_VERSION).tar
-release.s3:
+release.s3: release.add-attribution
 	$(call print-target)
 ifeq ($(CATALOG_APPLICATIONS_VERSION),"")
 	$(info CATALOG_APPLICATIONS_VERSION should be set to the version which is part of the s3 file path)
 else
+	TMP_BUNDLE_DIR="$(mktemp -d)"
+	mv $(CHART_BUNDLE) $(TMP_BUNDLE_DIR)/dkp-catalog-applications-charts-bundle-$(CATALOG_APPLICATIONS_VERSION).tar.gz
+	tar cvzf $(CHART_BUNDLE) NOTICES.txt -C $(TMP_BUNDLE_DIR) dkp-catalog-applications-charts-bundle-$(CATALOG_APPLICATIONS_VERSION).tar.gz
 	aws s3 cp --no-progress --acl bucket-owner-full-control $(CHART_BUNDLE) s3://$(RELEASE_S3_BUCKET)/dkp/$(CATALOG_APPLICATIONS_VERSION)/dkp-catalog-applications-charts-bundle-$(CATALOG_APPLICATIONS_VERSION).tar.gz
 	echo "Published Chart Bundle to $(CHART_BUNDLE_URL)"
 	aws s3 cp --no-progress --acl bucket-owner-full-control $(REPO_ARCHIVE_FILE) s3://$(RELEASE_S3_BUCKET)/dkp/$(CATALOG_APPLICATIONS_VERSION)/dkp-catalog-applications-$(CATALOG_APPLICATIONS_VERSION).tar.gz
 	echo "Published Repo Archive File to $(REPO_ARCHIVE_URL)"
-	aws s3 cp --no-progress --acl bucket-owner-full-control $(IMAGE_TAR_FILE) s3://$(RELEASE_S3_BUCKET)/dkp/$(CATALOG_APPLICATIONS_VERSION)/dkp-catalog-applications-image-bundle-$(CATALOG_APPLICATIONS_VERSION).tar
+	mv $(IMAGE_TAR_FILE) $(TMP_BUNDLE_DIR)/dkp-catalog-applications-image-bundle-$(CATALOG_APPLICATIONS_VERSION).tar
+	tar cvf $(IMAGE_TAR_FILE) NOTICES.txt -C $(TMP_BUNDLE_DIR)dkp-catalog-applications-image-bundle-$(CATALOG_APPLICATIONS_VERSION).tar
+	aws s3 cp --no-progress --acl bucket-owner-full-control $(IMAGE_TAR_FILE)  s3://$(RELEASE_S3_BUCKET)/dkp/$(CATALOG_APPLICATIONS_VERSION)/dkp-catalog-applications-image-bundle-$(CATALOG_APPLICATIONS_VERSION).tar
 	echo "Published Image Bundle to $(IMAGE_BUNDLE_URL)"
 ifeq (,$(findstring dev,$(CATALOG_APPLICATIONS_VERSION)))
 	# Make sure to set SLACK_WEBHOOK environment variable to webhook url for the below mentioned channel
@@ -58,3 +65,7 @@ ifeq (,$(findstring dev,$(CATALOG_APPLICATIONS_VERSION)))
 	$(SLACK_WEBHOOK)
 endif
 endif
+
+.PHONY: release.add-attribution
+release.add-attribution:
+	@echo $(ATTRIBUTION_FILE_CONTENT) > NOTICES.txt
