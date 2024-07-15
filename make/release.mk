da227853e09c6c5f3ea83deac09b7ee178cceaa0
Luke Ogg <luke.ogg@nutanix.com>
2024-07-15 14:59:07 -0600

    feat: Produce artifacts with both names

--- make/release.mk
+++ make/release.mk
@@ -1,7 +1,8 @@
 BUILD_DIR := _build
-IMAGE_TAR_FILE := $(BUILD_DIR)/nkp-catalog-applications-image-bundle.tar
-REPO_ARCHIVE_FILE := $(BUILD_DIR)/nkp-catalog-applications.tar.gz
-CHART_BUNDLE := $(BUILD_DIR)/nkp-catalog-applications-chart-bundle.tar.gz
+DKP_OR_NKP_PREFIX ?= nkp
+IMAGE_TAR_FILE := $(BUILD_DIR)/$(DKP_OR_NKP_PREFIX)-catalog-applications-image-bundle.tar
+REPO_ARCHIVE_FILE := $(BUILD_DIR)/$(DKP_OR_NKP_PREFIX)-catalog-applications.tar.gz
+CHART_BUNDLE := $(BUILD_DIR)/$(DKP_OR_NKP_PREFIX)-catalog-applications-chart-bundle.tar.gz
 CATALOG_IMAGES_TXT := $(BUILD_DIR)/nkp_catalog_images.txt
 CATALOG_IMAGES_TXT_WHITELISTED := $(BUILD_DIR)/nkp_catalog_images_whitelisted.txt
 RELEASE_S3_BUCKET ?= downloads.mesosphere.io
@@ -61,24 +62,24 @@ release.chart-bundle: kommander-cli
 		--output $(CHART_BUNDLE)
 
 .PHONY: release.s3
-release.s3: CHART_BUNDLE_URL = https://downloads.d2iq.com/dkp/$(CATALOG_APPLICATIONS_VERSION)/nkp-catalog-applications-charts-bundle-$(CATALOG_APPLICATIONS_VERSION).tar.gz
-release.s3: REPO_ARCHIVE_URL = https://downloads.d2iq.com/dkp/$(CATALOG_APPLICATIONS_VERSION)/nkp-catalog-applications-$(CATALOG_APPLICATIONS_VERSION).tar.gz
-release.s3: IMAGE_BUNDLE_URL = https://downloads.d2iq.com/dkp/$(CATALOG_APPLICATIONS_VERSION)/nkp-catalog-applications-image-bundle-$(CATALOG_APPLICATIONS_VERSION).tar
+release.s3: CHART_BUNDLE_URL = https://downloads.d2iq.com/dkp/$(CATALOG_APPLICATIONS_VERSION)/$(DKP_OR_NKP_PREFIX)-catalog-applications-charts-bundle-$(CATALOG_APPLICATIONS_VERSION).tar.gz
+release.s3: REPO_ARCHIVE_URL = https://downloads.d2iq.com/dkp/$(CATALOG_APPLICATIONS_VERSION)/$(DKP_OR_NKP_PREFIX)-catalog-applications-$(CATALOG_APPLICATIONS_VERSION).tar.gz
+release.s3: IMAGE_BUNDLE_URL = https://downloads.d2iq.com/dkp/$(CATALOG_APPLICATIONS_VERSION)/$(DKP_OR_NKP_PREFIX)-catalog-applications-image-bundle-$(CATALOG_APPLICATIONS_VERSION).tar
 release.s3: release.add-attribution
 	$(call print-target)
 ifeq ($(CATALOG_APPLICATIONS_VERSION),"")
 	$(info CATALOG_APPLICATIONS_VERSION should be set to the version which is part of the s3 file path)
 else
 	mkdir -p $(BUILD_DIR)/tmp
-	mv $(CHART_BUNDLE) $(BUILD_DIR)/tmp/nkp-catalog-applications-charts-bundle-$(CATALOG_APPLICATIONS_VERSION).tar.gz
-	tar cvzf $(CHART_BUNDLE) NOTICES.txt -C $(BUILD_DIR)/tmp nkp-catalog-applications-charts-bundle-$(CATALOG_APPLICATIONS_VERSION).tar.gz
-	aws s3 cp --no-progress --acl bucket-owner-full-control $(CHART_BUNDLE) s3://$(RELEASE_S3_BUCKET)/dkp/$(CATALOG_APPLICATIONS_VERSION)/nkp-catalog-applications-charts-bundle-$(CATALOG_APPLICATIONS_VERSION).tar.gz
+	mv $(CHART_BUNDLE) $(BUILD_DIR)/tmp/$(DKP_OR_NKP_PREFIX)-catalog-applications-charts-bundle-$(CATALOG_APPLICATIONS_VERSION).tar.gz
+	tar cvzf $(CHART_BUNDLE) NOTICES.txt -C $(BUILD_DIR)/tmp $(DKP_OR_NKP_PREFIX)-catalog-applications-charts-bundle-$(CATALOG_APPLICATIONS_VERSION).tar.gz
+	aws s3 cp --no-progress --acl bucket-owner-full-control $(CHART_BUNDLE) s3://$(RELEASE_S3_BUCKET)/dkp/$(CATALOG_APPLICATIONS_VERSION)/$(DKP_OR_NKP_PREFIX)-catalog-applications-charts-bundle-$(CATALOG_APPLICATIONS_VERSION).tar.gz
 	echo "Published Chart Bundle to $(CHART_BUNDLE_URL)"
-	aws s3 cp --no-progress --acl bucket-owner-full-control $(REPO_ARCHIVE_FILE) s3://$(RELEASE_S3_BUCKET)/dkp/$(CATALOG_APPLICATIONS_VERSION)/nkp-catalog-applications-$(CATALOG_APPLICATIONS_VERSION).tar.gz
+	aws s3 cp --no-progress --acl bucket-owner-full-control $(REPO_ARCHIVE_FILE) s3://$(RELEASE_S3_BUCKET)/dkp/$(CATALOG_APPLICATIONS_VERSION)/$(DKP_OR_NKP_PREFIX)-catalog-applications-$(CATALOG_APPLICATIONS_VERSION).tar.gz
 	echo "Published Repo Archive File to $(REPO_ARCHIVE_URL)"
-	mv $(IMAGE_TAR_FILE) $(BUILD_DIR)/tmp/nkp-catalog-applications-image-bundle-$(CATALOG_APPLICATIONS_VERSION).tar
-	tar cvf $(IMAGE_TAR_FILE) NOTICES.txt -C $(BUILD_DIR)/tmp nkp-catalog-applications-image-bundle-$(CATALOG_APPLICATIONS_VERSION).tar
-	aws s3 cp --no-progress --acl bucket-owner-full-control $(IMAGE_TAR_FILE)  s3://$(RELEASE_S3_BUCKET)/dkp/$(CATALOG_APPLICATIONS_VERSION)/nkp-catalog-applications-image-bundle-$(CATALOG_APPLICATIONS_VERSION).tar
+	mv $(IMAGE_TAR_FILE) $(BUILD_DIR)/tmp/$(DKP_OR_NKP_PREFIX)-catalog-applications-image-bundle-$(CATALOG_APPLICATIONS_VERSION).tar
+	tar cvf $(IMAGE_TAR_FILE) NOTICES.txt -C $(BUILD_DIR)/tmp $(DKP_OR_NKP_PREFIX)-catalog-applications-image-bundle-$(CATALOG_APPLICATIONS_VERSION).tar
+	aws s3 cp --no-progress --acl bucket-owner-full-control $(IMAGE_TAR_FILE)  s3://$(RELEASE_S3_BUCKET)/dkp/$(CATALOG_APPLICATIONS_VERSION)/$(DKP_OR_NKP_PREFIX)-catalog-applications-image-bundle-$(CATALOG_APPLICATIONS_VERSION).tar
 	echo "Published Image Bundle to $(IMAGE_BUNDLE_URL)"
 ifeq (,$(findstring dev,$(CATALOG_APPLICATIONS_VERSION)))
 	# Make sure to set SLACK_WEBHOOK environment variable to webhook url for the below mentioned channel
